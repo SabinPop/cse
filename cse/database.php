95e71aee20fb37d4066e2a1ccd87043fa311aec4
SabinPop <32741414+SabinPop@users.noreply.github.com>
2017-10-12 18:04:07 +0300

    Add files via upload

---
+++ cse/database.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'cseunire_members',
+    'user' => 'cseunire',
+    'password' => 'PPOE3217GFEm',
+    'prefix' => 'postleaf_'
+];
